0817b49b5df6bdedab3293a607cc551d67c4944d
MingHHsu <ming.hs.hsu@gmail.com>
2021-06-14 00:16:47 +0800

    process kill

--- src/server.js
+++ src/server.js
@@ -11,6 +11,13 @@ async function startServer () {
   server.listen(config.port, () => {
     console.log(`Server listening on port: ${config.port}`);
   }).on('error', err => {
+    process.once('SIGUSR2', function () {
+      process.kill(process.pid, 'SIGUSR2');
+    });
+
+    process.on('SIGINT', function () {
+      process.kill(process.pid, 'SIGINT');
+    });
     console.error(err);
     process.exit(1);
   });
